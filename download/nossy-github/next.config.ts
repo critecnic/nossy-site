7076b625488b2c7d6dac03a7c92ce548806ee498
Z User <z@container>
2026-08-13 22:51:30 +0000

    898d525f-cf12-42a7-a123-6170e345cebc

---
+++ download/nossy-github/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  reactStrictMode: false,
+};
+
+export default nextConfig;
